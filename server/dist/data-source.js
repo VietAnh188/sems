b100550098f9b64cdecadde7252ecb0eb6a28673
VietAnh188 <anhkun123456@gmail.com>
2022-05-21 20:50:32 +0700

    initial

--- server/dist/data-source.js
+++ server/dist/data-source.js
@@ -9,11 +9,11 @@ const Person_1 = require("./entities/Person");
 const Role_1 = require("./entities/Role");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: 'sems',
+    host: process.env.PRODUCTION_HOST || 'localhost',
+    port: Number(process.env.PRODUCTION_PORT) || 5432,
+    username: process.env.PRODUCTION_USERNAME || process.env.POSTGRES_USERNAME,
+    password: process.env.PRODUCTION_PASSWORD || process.env.POSTGRES_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE || 'sems',
     synchronize: true,
     logging: true,
     entities: [Account_1.Account, Person_1.Person, Role_1.Role, Department_1.Department, Ethnicity_1.Ethnicity],
--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -7,11 +7,11 @@ import { Role } from './entities/Role';
 
 export const AppDataSource: DataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: 'sems',
+    host: process.env.PRODUCTION_HOST || 'localhost',
+    port: Number(process.env.PRODUCTION_PORT) || 5432,
+    username: process.env.PRODUCTION_USERNAME || process.env.POSTGRES_USERNAME,
+    password: process.env.PRODUCTION_PASSWORD || process.env.POSTGRES_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE || 'sems',
     synchronize: true,
     logging: true,
     entities: [Account, Person, Role, Department, Ethnicity],
